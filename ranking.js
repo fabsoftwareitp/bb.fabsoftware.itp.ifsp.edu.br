5097429b07016a586428056e631704dd16166630
rpr99 <rafaelpassosrod@gmail.com>
2025-05-14 17:07:19 -0300

    alteração da variavel do ranking para minuscula

--- ranking.js
+++ ranking.js
@@ -13,8 +13,8 @@ function createRankingList(rankingJson) {
   rankingList.innerHTML = null;
   let aux = 0;
   rankingJson.sort(function(a, b){return b.score - a.score}); //ordenando
-  const Top10players = rankingJson.slice(0, 10);
-  Top10players.forEach(player => {
+  const top10players = rankingJson.slice(0, 10);
+  top10players.forEach(player => {
     const item = createPlayerElement(player);
     rankingList.appendChild(item);
   });
